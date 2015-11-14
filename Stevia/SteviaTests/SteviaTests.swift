96c5e9ddb247c05aeaee8036e81416b7e3d3ff5b
Sacha Durand Saint Omer <sachadso@gmail.com>
2015-11-14 13:15:24 +0100

    Adds test target

---
+++ Stevia/SteviaTests/SteviaTests.swift
@@ -0,0 +1,35 @@
+//
+//  SteviaTests.swift
+//  SteviaTests
+//
+//  Created by Sacha Durand Saint Omer on 14/11/15.
+//  Copyright © 2015 Sacha Durand Saint Omer. All rights reserved.
+//
+
+import XCTest
+
+class SteviaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
