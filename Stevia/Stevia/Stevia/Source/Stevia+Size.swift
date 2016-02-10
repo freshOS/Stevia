b9d2b6cefc858d177761b1acf5f36992419a3d6b
Sacha Durand Saint Omer <sachadso@gmail.com>
2016-02-10 21:31:54 +0100

    Refactors size methods

--- Stevia/Stevia/Stevia/Source/Stevia+Size.swift
+++ Stevia/Stevia/Stevia/Source/Stevia+Size.swift
@@ -10,21 +10,24 @@ import Foundation
 
 public extension UIView {
     
-    public func height(points:CGFloat) -> UIView {
-        let c = constraint(item: self, attribute: .Height, constant: points)
-        addConstraint(c)
-        return self
-    }
-    
-    public func width(points:CGFloat) -> UIView {
-        let c = constraint(item: self, attribute: .Width, constant: points)
-        addConstraint(c)
-        return self
-    }
-    
     public func size(points:CGFloat) -> UIView {
         width(points)
         height(points)
         return self
     }
+    
+    public func height(points:CGFloat) -> UIView {
+        return size(.Height, points: points)
+    }
+    
+    public func width(points:CGFloat) -> UIView {
+        return size(.Width, points: points)
+    }
+    
+    public func size(attribute:NSLayoutAttribute, points:CGFloat) -> UIView {
+        let c = constraint(item: self, attribute:attribute, constant: points)
+        addConstraint(c)
+        return self
+    }
+    
 }
