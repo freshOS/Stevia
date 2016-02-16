54ef54d4e8885c4f890e4253118e8a6ff66d9e90
s4cha <sachadso@gmail.com>
2016-02-16 22:50:52 +0100

    Adds Playground so people can play around with Stevia easily

---
+++ Stevia/Playground/SteviaPG.playground/Contents.swift
@@ -0,0 +1,59 @@
+//: Playground - noun: a place where people can play
+import UIKit
+import XCPlayground
+import Stevia
+
+// Open the Workspace and
+// play around with the constraints! 
+
+class LoginViewStevia:UIView {
+    
+    let email = UITextField()
+    let password = UITextField()
+    let login = UIButton()
+    
+    convenience init() {
+        self.init(frame:CGRectZero)
+        backgroundColor = .darkGrayColor()
+        
+        sv([
+            email.placeholder("Email").style(fieldStyle),
+            password.placeholder("Password").style(fieldStyle).style(passwordFieldStyle),
+            login.text("Login").style(buttonSytle).tap(loginTapped)
+        ])
+        
+        layout([
+            100,
+            |-email-| ~ 80,
+            8,
+            |-password-| ~ 80,
+            "",
+            |login| ~ 80,
+            0
+        ])
+    }
+    
+    func fieldStyle(f:UITextField) {
+        f.borderStyle = .RoundedRect
+        f.font = UIFont(name: "HelveticaNeue-Light", size: 26)
+        f.returnKeyType = .Next
+    }
+    
+    func passwordFieldStyle(f:UITextField) {
+        f.secureTextEntry = true
+        f.returnKeyType = .Done
+    }
+    
+    func buttonSytle(b:UIButton) {
+        b.backgroundColor = .lightGrayColor()
+    }
+    
+    func loginTapped() {
+        //Do something
+    }
+}
+
+let v = LoginViewStevia()
+v.frame = CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0)
+XCPlaygroundPage.currentPage.liveView = v
+
