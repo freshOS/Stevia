9378cbaa62c6bcf3e48af19849a51319ee2e286a
Sacha Durand Saint Omer <sachadso@gmail.com>
2016-02-23 12:10:33 +0100

    Fix layout array bottom margin

--- Stevia/Stevia/Stevia/Source/Stevia+Stacks.swift
+++ Stevia/Stevia/Stevia/Source/Stevia+Stacks.swift
@@ -45,7 +45,7 @@ public extension UIView {
                     //Last Margin, Bottom
                     if let previousView = objects[i-1] as? UIView {
                         previousView.bottom(m)
-                    } else if let va = objects[i-2] as? [UIView] {
+                    } else if let va = objects[i-1] as? [UIView] {
                         va.first!.bottom(m)
                     }
                 }
