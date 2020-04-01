42f39154df90aceffa8c8e86e4e20c2b7d16ba34
S4cha <sachadso@gmail.com>
2020-04-01 10:33:15 +0200

    Adds "Subviews" function builder + tests

--- Tests/SteviaTests/HierarchyTests.swift
+++ Tests/SteviaTests/HierarchyTests.swift
@@ -34,21 +34,6 @@ class HierarchyTests: XCTestCase {
         XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
     }
 
-    func testVariadicSv() {
-        let view = UIView()
-        let v1 = UIView()
-        let v2 = UIView()
-        view.sv(
-            v1,
-            v2
-            )
-        XCTAssertEqual(view.subviews.count, 2)
-        XCTAssertTrue(view.subviews.contains(v1))
-        XCTAssertTrue(view.subviews.contains(v2))
-        XCTAssertFalse(v1.translatesAutoresizingMaskIntoConstraints)
-        XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
-    }
-
     func testTableViewCellSV() {
         let cell = UITableViewCell()
         let v1 = UIView()
@@ -56,29 +41,14 @@ class HierarchyTests: XCTestCase {
         cell.sv(
             v1,
             v2
-            )
+        )
         XCTAssertEqual(cell.contentView.subviews.count, 2)
         XCTAssertTrue(cell.contentView.subviews.contains(v1))
         XCTAssertTrue(cell.contentView.subviews.contains(v2))
         XCTAssertFalse(v1.translatesAutoresizingMaskIntoConstraints)
         XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
     }
-
-    func testTableViewCellVariadicSV() {
-        let cell = UITableViewCell()
-        let v1 = UIView()
-        let v2 = UIView()
-        cell.sv(
-            v1,
-            v2
-            )
-        XCTAssertEqual(cell.contentView.subviews.count, 2)
-        XCTAssertTrue(cell.contentView.subviews.contains(v1))
-        XCTAssertTrue(cell.contentView.subviews.contains(v2))
-        XCTAssertFalse(v1.translatesAutoresizingMaskIntoConstraints)
-        XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
-    }
-
+    
     func testCollectionViewCellSV() {
         let cell = UICollectionViewCell()
         let v1 = UIView()
@@ -93,15 +63,50 @@ class HierarchyTests: XCTestCase {
         XCTAssertFalse(v1.translatesAutoresizingMaskIntoConstraints)
         XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
     }
+    
+    // Function Builders version
+    
+    func testSubviews() {
+        let view = UIView()
+        let v1 = UIView()
+        let v2 = UIView()
+        view.Subviews {
+            v1
+            v2
+        }
+        XCTAssertEqual(view.subviews.count, 2)
+        XCTAssertTrue(view.subviews.contains(v1))
+        XCTAssertTrue(view.subviews.contains(v2))
+        XCTAssertFalse(v1.translatesAutoresizingMaskIntoConstraints)
+        XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
+    }
 
-    func testCollectionViewCellVariadicSV() {
+    
+    func testTableViewCellSubviews() {
+        let cell = UITableViewCell()
+        let v1 = UIView()
+        let v2 = UIView()
+        cell.Subviews {
+            v1
+            v2
+        }
+            
+        
+        XCTAssertEqual(cell.contentView.subviews.count, 2)
+        XCTAssertTrue(cell.contentView.subviews.contains(v1))
+        XCTAssertTrue(cell.contentView.subviews.contains(v2))
+        XCTAssertFalse(v1.translatesAutoresizingMaskIntoConstraints)
+        XCTAssertFalse(v2.translatesAutoresizingMaskIntoConstraints)
+    }
+
+    func testCollectionViewCellSubviews() {
         let cell = UICollectionViewCell()
         let v1 = UIView()
         let v2 = UIView()
-        cell.sv(
-            v1,
+        cell.Subviews {
+            v1
             v2
-            )
+        }
         XCTAssertEqual(cell.contentView.subviews.count, 2)
         XCTAssertTrue(cell.contentView.subviews.contains(v1))
         XCTAssertTrue(cell.contentView.subviews.contains(v2))
