801b0100d6ce78627eb741ce487d00bef4dd1c31
Scott Bates <scott.bates@22greystreet.com>
2022-05-19 17:02:13 -0700

    remove deprecation warnings on all -- operators

--- Sources/Stevia/Stevia+DoubleDash.swift
+++ Sources/Stevia/Stevia+DoubleDash.swift
@@ -12,127 +12,106 @@ import UIKit
 infix operator -- :AdditionPrecedence
 
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: UIView, right: Double) -> PartialConstraint {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 public func -- (left: UIView, right: CGFloat) -> PartialConstraint {
     return left--Double(right)
 }
 
-@available(*, deprecated, renamed: "⁃")
 public func -- (left: UIView, right: Int) -> PartialConstraint {
     return left--Double(right)
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: SideConstraint, right: UIView) -> UIView {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: SideConstraint) -> [UIView] {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: UIView, right: SideConstraint) -> UIView {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: PartialConstraint, right: UIView) -> [UIView] {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: UIView, right: UIView) -> [UIView] {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: Double) -> PartialConstraint {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: CGFloat) -> PartialConstraint {
     return left--Double(right)
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: Int) -> PartialConstraint {
     return left--Double(right)
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: UIView) -> [UIView] {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: UIView, right: String) -> Space {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: String) -> Space {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: Space, right: UIView) -> [UIView] {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: UIView,
                 right: SteviaFlexibleMargin) -> PartialFlexibleConstraint {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView],
                 right: SteviaFlexibleMargin) -> PartialFlexibleConstraint {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: PartialFlexibleConstraint, right: UIView) -> [UIView] {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: SteviaLeftFlexibleMargin, right: UIView) -> UIView {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: UIView, right: SteviaRightFlexibleMargin) -> UIView {
     return left-right
 }
 
-@available(*, deprecated, renamed: "⁃")
 @discardableResult
 public func -- (left: [UIView], right: SteviaRightFlexibleMargin) -> [UIView] {
     return left-right
